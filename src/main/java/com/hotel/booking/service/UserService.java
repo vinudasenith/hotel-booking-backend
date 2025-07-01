f68a36dce1352ee638e1d5147e5481b7e5d568f9
vinudasenith <ha.vinudas@gmail.com>
2025-07-01 19:49:17 +0530

    implement User service👍

---
+++ src/main/java/com/hotel/booking/service/UserService.java
@@ -0,0 +1,47 @@
+package com.hotel.booking.service;
+
+import com.hotel.booking.model.User;
+import com.hotel.booking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            return null;
+        } else {
+            return userRepository.save(user);
+        }
+    }
+
+    public User loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean isAdmin(String email) {
+        User user = userRepository.findByEmail(email);
+        return user != null && user.getRole().equals("admin");
+    }
+
+    public boolean isCustomer(String email) {
+        User user = userRepository.findByEmail(email);
+        return user != null && user.getRole().equals("customer");
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
