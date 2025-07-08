0388d2e84974d1295bd55cef382c48d5ab111f54
vinudasenith <ha.vinudas@gmail.com>
2025-07-08 10:29:22 +0530

    implement booking controller👍

---
+++ src/main/java/com/hotel/booking/service/BookingService.java
@@ -0,0 +1,62 @@
+package com.hotel.booking.service;
+
+import com.hotel.booking.model.Booking;
+import com.hotel.booking.repository.BookingRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public Booking createBooking(Booking booking) {
+        Booking lastBooking = bookingRepository.findTopByOrderByBookingIdDesc();
+
+        String nextId = "BOOKING-001";
+
+        if (lastBooking != null && lastBooking.getBookingId() != null) {
+            String lastId = lastBooking.getBookingId();
+            int num = Integer.parseInt(lastId.substring(3));
+            num++;
+            nextId = String.format("ORD%03d", num);
+        }
+        booking.setBookingId(nextId);
+        return bookingRepository.save(booking);
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    public boolean deleteBooking(String bookingId) {
+        if (bookingRepository.existsById(bookingId)) {
+            bookingRepository.deleteById(bookingId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Booking updateBooking(String bookingId, Booking newBookinhg) {
+        if (bookingRepository.existsById(bookingId)) {
+            newBookinhg.setBookingId(bookingId);
+            return bookingRepository.save(newBookinhg);
+        } else {
+            return null;
+        }
+    }
+
+    public List<Booking> getBookingsById(int roomId) {
+        return bookingRepository.findByRoomId(roomId);
+    }
+
+    public List<Booking> getBookingsByEmail(String guestEmail) {
+        return bookingRepository.findByGuestEmail(guestEmail);
+    }
+
+}
