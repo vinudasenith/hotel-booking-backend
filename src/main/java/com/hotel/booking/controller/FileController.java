31faee075e5f34912a0454d881cb3e9438647ed5
vinudasenith <ha.vinudas@gmail.com>
2025-07-15 20:56:17 +0530

    implement functionality for storing photos👍

---
+++ src/main/java/com/hotel/booking/controller/FileController.java
@@ -0,0 +1,25 @@
+package com.hotel.booking.controller;
+
+import com.hotel.booking.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/files")
+@CrossOrigin(origins = "http://localhost:8080")
+public class FileController {
+
+    @Autowired
+    private StorageService storageService;
+
+    @GetMapping("/uploads/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+}
