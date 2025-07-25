db166215c1b5ef89c6712538cfd5d350c550e01c
vinudasenith <ha.vinudas@gmail.com>
2025-07-25 22:51:23 +0530

    add vercel frontend domain👍

--- src/main/java/com/hotel/booking/config/CorsConfig.java
+++ src/main/java/com/hotel/booking/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://hotel-booking-inky-chi.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
