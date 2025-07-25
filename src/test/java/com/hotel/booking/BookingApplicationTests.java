8feec32fb2063d33bcf42b3bb1ea13a53a4c15e5
vinudasenith <ha.vinudas@gmail.com>
2025-07-25 22:10:24 +0530

    implement bookingApplicationTests.java👍

--- src/test/java/com/hotel/booking/BookingApplicationTests.java
+++ src/test/java/com/hotel/booking/BookingApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookingApplicationTests {
+public class BookingApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
