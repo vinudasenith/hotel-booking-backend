40aae8c284ab39d4795d4be5936774a47caf262e
vinudasenith <ha.vinudas@gmail.com>
2025-09-05 22:05:33 +0530

    return jwt token 👍

--- src/main/java/com/hotel/booking/controller/UserController.java
+++ src/main/java/com/hotel/booking/controller/UserController.java
@@ -2,6 +2,8 @@ package com.hotel.booking.controller;
 
 import com.hotel.booking.model.User;
 import com.hotel.booking.service.UserService;
+import com.hotel.booking.util.JwtUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
@@ -13,15 +15,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = { "http://localhost:8080", "hotel-booking-frontend-omega.vercel.app",
-        "hotel-booking-frontend-git-master-vinudas-projects.vercel.app",
-        "hotel-booking-frontend-dhs6wk2gy-vinudas-projects.vercel.app" })
+@CrossOrigin(origins = { "http://localhost:8080" })
 
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    // register user
     @PostMapping("/register")
     public ResponseEntity<Map<String, String>> registerUser(@RequestBody User user) {
         User savedUser = userService.registerUser(user);
@@ -36,31 +40,45 @@ public class UserController {
         }
     }
 
+    // user login
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody User user) {
         User loggedInUser = userService.loginUser(user.getEmail(), user.getPassword());
+        Map<String, Object> response = new HashMap<>();
+
         if (loggedInUser != null) {
-            return ResponseEntity.ok(loggedInUser);
+
+            // Generate JWT
+            String token = jwtUtil.generateToken(loggedInUser.getEmail());
+            response.put("message", "Login successful");
+            response.put("token", token);
+            response.put("user", loggedInUser);
+
+            return ResponseEntity.ok(response);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
         }
     }
 
+    // check user role (is it admin?)
     @GetMapping("/is-admin/{email}")
     public boolean isAdmin(@PathVariable String email) {
         return userService.isAdmin(email);
     }
 
+    // check user role (is it customer?)
     @GetMapping("/is-customer/{email}")
     public boolean isCustomer(@PathVariable String email) {
         return userService.isCustomer(email);
     }
 
+    // get all users
     @GetMapping("/all")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    // block user by email
     @PutMapping("/block/{email}")
     public ResponseEntity<String> toggleUserStatus(@PathVariable String email) {
         User user = userService.findByEmail(email);
